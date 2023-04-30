d078abbd54002450f9261824ac49a7c39bf0259c
William A Loring <32806514+itinstructor@users.noreply.github.com>
2023-04-29 20:48:32 -0600

    First commit.

--- src/main/java/com/billthecomputerguy/noteblockplugin/Main.java
+++ src/main/java/com/billthecomputerguy/noteblockplugin/Main.java
@@ -2,7 +2,6 @@ package com.billthecomputerguy.noteblockplugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
-
     @Override
     public void onEnable() {
         // The data folder is the name of the plugin, NoteBlockSongs
